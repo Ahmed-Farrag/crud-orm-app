364fc53f422e12970f8b5c1eed9518f64894db04
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-03-02 09:14:34 +0200

    repo interface

--- road.js
+++ road.js
@@ -3,7 +3,11 @@ create project
 create resource/crud 
 
 
-*repositry pattern
+*repositry pattern : كنت كل شوية اكتب جمله الكويري بتعتي فهنا بعملها في انترفيس واحده وباخدها في كل روت
 make : interface تمثل skeleton or blueprint for class
-make folder repositories/base/base.interface.ts
+make folder repositories/base/base.interface.ts and write function
+
+install typeorm and import it 
+>npm i typeorm
+import {DeleteResult} from 'typeorm'
 */
--- src/repositories/base/base.interface.ts
+++ src/repositories/base/base.interface.ts
@@ -1,3 +1,9 @@
+import { DeleteResult } from 'typeorm';
 export interface BaseInterface<T> {
   findOneById(id: number): Promise<T>;
+  findAll(): Promise<T[]>;
+  create(data: T | any): Promise<T>;
+  findByCondition(filterCondition: any): Promise<T>;
+  remove(id: string): Promise<DeleteResult>;
+  findWithRelations(relations: any): Promise<T[]>;
 }
